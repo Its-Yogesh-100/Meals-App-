8808147a5e171f51964390ee3021cb52a9792741
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2025-12-30 14:51:29 +0530

    added loading screen functionality and IntroScreen component

--- Calculator/screens/MainScreen.js
+++ Calculator/screens/MainScreen.js
@@ -60,6 +60,86 @@ const MainScreen = () => {
             </View>
           </Pressable>
         </View>
+
+        <View style={styles.keypad_row}>
+          <Pressable>
+            <View style={styles.btn1_outer}>
+              <Text style={styles.bg_button}>7</Text>
+            </View>
+          </Pressable>
+
+          <Pressable>
+            <View style={styles.btn1_outer}>
+              <Text style={styles.bg_button}>8</Text>
+            </View>
+          </Pressable>
+
+          <Pressable>
+            <View style={styles.btn1_outer}>
+              <Text style={styles.bg_button}>9</Text>
+            </View>
+          </Pressable>
+
+          <Pressable>
+            <View style={styles.btn1_outer}>
+              <Text style={styles.bg_button}>/</Text>
+            </View>
+          </Pressable>
+        </View>
+
+        <View style={styles.keypad_row}>
+          <Pressable>
+            <View style={styles.btn1_outer}>
+              <Text style={styles.bg_button}>7</Text>
+            </View>
+          </Pressable>
+
+          <Pressable>
+            <View style={styles.btn1_outer}>
+              <Text style={styles.bg_button}>8</Text>
+            </View>
+          </Pressable>
+
+          <Pressable>
+            <View style={styles.btn1_outer}>
+              <Text style={styles.bg_button}>9</Text>
+            </View>
+          </Pressable>
+
+          <Pressable>
+            <View style={styles.btn1_outer}>
+              <Text style={styles.bg_button}>/</Text>
+            </View>
+          </Pressable>
+        </View>
+
+        <View style={styles.keypad_row}>
+          <Pressable>
+            <View style={styles.btn1_outer}>
+              <Text style={styles.bg_button}>7</Text>
+            </View>
+          </Pressable>
+
+          <Pressable>
+            <View style={styles.btn1_outer}>
+              <Text style={styles.bg_button}>8</Text>
+            </View>
+          </Pressable>
+
+          <Pressable>
+            <View style={styles.btn1_outer}>
+              <Text style={styles.bg_button}>9</Text>
+            </View>
+          </Pressable>
+
+          <Pressable>
+            <View style={styles.btn1_outer}>
+              <Text style={styles.bg_button}>/</Text>
+            </View>
+          </Pressable>
+        </View>
+
+
       </View>
     </View>
   );
@@ -67,4 +147,61 @@ const MainScreen = () => {
 
 export default MainScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+
+  main_screen: {
+   
+    marginTop: 50,
+   display: "flex",
+   flexDirection: "column",
+   justifyContent: "space-between",
+   alignItems: "center",
+   height: "100%",
+   width: "100%",
+  },
+
+  scroll_view: {
+    elevation: 10,
+    width: "95%",
+    backgroundColor:"white",
+    borderRadius:10,
+    display:"flex",
+    marginBottom:10,
+    padding:10,
+  },
+  main_screen_display_text: {
+    fontSize: 50,
+    textAlign: "right",
+    color: "black",
+  },
+
+  main_keypad: {
+    width: "100%",
+    height: "70%",
+    display: "flex",
+    flexDirection: "column",
+    
+    alignItems: "center",
+    
+  },
+  keypad_row: {
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: "#fcb6b6ff",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+  },
+  btn1_outer: { 
+    width: 90,
+    height: 90,
+    backgroundColor:'yellow',
+    elevation: 10,
+    overflow: "hidden",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 30
+  }
+
+});
